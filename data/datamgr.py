2d04e374e13df49a2804a791bd62315c06ce8f2a
Szymon Dziedzic <sz.dziedz@gmail.com>
2023-05-06 11:18:36 +0200

    fix num of workers in dataloaders

--- data/datamgr.py
+++ data/datamgr.py
@@ -10,24 +10,24 @@ from abc import abstractmethod
 
 def _init_fn(worker_id):
     np.random.seed(0)
-    
+
 class TransformLoader:
-    def __init__(self, image_size, 
+    def __init__(self, image_size,
                  normalize_param    = dict(mean= [0.485, 0.456, 0.406] , std=[0.229, 0.224, 0.225]),
                  jitter_param       = dict(Brightness=0.4, Contrast=0.4, Color=0.4)):
         self.image_size = image_size
         self.normalize_param = normalize_param
         self.jitter_param = jitter_param
-    
+
     def parse_transform(self, transform_type):
         if transform_type=='ImageJitter':
             method = add_transforms.ImageJitter( self.jitter_param )
             return method
         method = getattr(transforms, transform_type)
         if transform_type=='RandomResizedCrop':
-            return method(self.image_size) 
+            return method(self.image_size)
         elif transform_type=='CenterCrop':
-            return method(self.image_size) 
+            return method(self.image_size)
         elif transform_type=='Resize':
             return method([int(self.image_size*1.15), int(self.image_size*1.15)])
         elif transform_type=='Normalize':
@@ -48,11 +48,11 @@ class TransformLoader:
 class DataManager:
     @abstractmethod
     def get_data_loader(self, data_file, aug):
-        pass 
+        pass
 
 
 class SimpleDataManager(DataManager):
-    def __init__(self, image_size, batch_size):        
+    def __init__(self, image_size, batch_size):
         super(SimpleDataManager, self).__init__()
         self.batch_size = batch_size
         self.trans_loader = TransformLoader(image_size)
@@ -61,14 +61,14 @@ class SimpleDataManager(DataManager):
         transform = self.trans_loader.get_composed_transform(aug)
         dataset = SimpleDataset(data_file, transform)
 
-        data_loader_params = dict(batch_size = self.batch_size, shuffle=True, num_workers = 8, pin_memory=True)       
+        data_loader_params = dict(batch_size = self.batch_size, shuffle=True, num_workers = 2, pin_memory=True)
 
         data_loader = torch.utils.data.DataLoader(dataset, **data_loader_params)
 
         return data_loader
 
 class SetDataManager(DataManager):
-    def __init__(self, image_size, n_way, n_support, n_query, n_eposide =100):        
+    def __init__(self, image_size, n_way, n_support, n_query, n_eposide =100):
         super(SetDataManager, self).__init__()
         self.image_size = image_size
         self.n_way = n_way
@@ -80,9 +80,9 @@ class SetDataManager(DataManager):
     def get_data_loader(self, data_file, aug): #parameters that would change on train/val set
         transform = self.trans_loader.get_composed_transform(aug)
         dataset = SetDataset( data_file , self.batch_size, transform )
-        sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide )  
+        sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide )
 
-        data_loader_params = dict(batch_sampler = sampler, num_workers = 8, pin_memory=True)
+        data_loader_params = dict(batch_sampler = sampler, num_workers = 2, pin_memory=True)
         data_loader = torch.utils.data.DataLoader(dataset, **data_loader_params)
         return data_loader
 
